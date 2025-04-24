96cd8a2cd9fdc1474676e11e8dfe4af3d82be9d7
funchooooza-ossh <uroddaunich@gmail.com>
2025-04-25 07:18:16 +1000

    upd: типы

--- fileferry/src/composition/uow/factory.py
+++ fileferry/src/composition/uow/factory.py
@@ -1,8 +1,10 @@
-from collections.abc import AsyncGenerator, Callable
+from collections.abc import Callable
+from contextlib import AbstractAsyncContextManager
 from typing import Any, Literal
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from contracts.application import UnitOfWork
 from infrastructure.db.session import get_async_session
 from infrastructure.uow.sqlalchemy_file import SQLAlchemyUnitOfWork
 
@@ -11,18 +13,18 @@ KnownUoW = Literal["sqla", "mongo"]
 
 class UnitOfWorkFactory:
     @classmethod
-    def create(cls, config: KnownUoW, **kwargs: Any) -> Callable:
+    def create(cls, config: KnownUoW, **kwargs: Any) -> UnitOfWork:
         match config:
             case "sqla":
                 return cls.create_sqlalchemy(**kwargs)
             case "mongo":
-                return
+                raise NotImplementedError("Mongo not implemented")
             case _:
                 raise ValueError("Unknown UoW")
 
     @staticmethod
     def create_sqlalchemy(
-        session_factory: AsyncGenerator[AsyncSession, None] = get_async_session,
+        session_factory: Callable[[], AbstractAsyncContextManager[AsyncSession]] = get_async_session,
     ) -> SQLAlchemyUnitOfWork:
         return SQLAlchemyUnitOfWork(
             session_factory=session_factory,
