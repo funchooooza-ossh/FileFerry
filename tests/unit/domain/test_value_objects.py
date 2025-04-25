7db7fc76a870f3451a43d5b0ca4425992ee63038
funchooooza <mesherinovn@yahoo.com>
2025-04-25 10:13:30 +1000

    upd: value objects tests

--- tests/unit/domain/test_value_objects.py
+++ tests/unit/domain/test_value_objects.py
@@ -2,7 +2,7 @@ import random
 import uuid
 
 import pytest
-from domain.models.value_objects import FileId, FileName
+from domain.models.value_objects import ContentType, FileId, FileName, FileSize
 
 
 @pytest.mark.asyncio
@@ -29,9 +29,8 @@ async def test_file_id_initialize_not_valid_uuid():
     with pytest.raises(ValueError, match="Invalid UUID"):
         file_id_vo = FileId(value=file_id)
 
-        assert (
-            file_id_vo is not None
-        )  # инициализировался, но выбросил ошибку на пост ините
+        assert file_id_vo is not None
+        # инициализировался, но выбросил ошибку на пост ините
 
 
 @pytest.mark.asyncio
@@ -73,3 +72,53 @@ async def test_filename_empty():
         filename = FileName(value=string_name)
 
         assert filename is not None
+
+
+@pytest.mark.asyncio
+async def test_content_type_initialize():
+    string_type = "application/pdf"
+
+    content_type_vo = ContentType(value=string_type)
+
+    assert content_type_vo is not None
+    assert content_type_vo.value == string_type
+
+
+@pytest.mark.asyncio
+async def test_content_type_invalid_format():
+    string_type = "invalidtype"
+
+    with pytest.raises(ValueError, match="Invalid MIME"):
+        content_type_vo = ContentType(value=string_type)
+
+        assert content_type_vo is not None
+
+
+@pytest.mark.asyncio
+async def test_content_type_incomplete():
+    string_type = "invalidtype/"
+
+    with pytest.raises(ValueError, match="Incomplete MIME"):
+        content_type_vo = ContentType(value=string_type)
+
+        assert content_type_vo is not None
+
+
+@pytest.mark.asyncio
+async def test_file_size_initialize():
+    size = 134
+
+    file_size = FileSize(value=size)
+
+    assert file_size is not None
+    assert file_size.value == size
+
+
+@pytest.mark.asyncio
+async def test_file_size_negative():
+    size = -5
+
+    with pytest.raises(ValueError, match="File size must be positive"):
+        file_size = FileSize(value=size)
+
+        assert file_size is not None
