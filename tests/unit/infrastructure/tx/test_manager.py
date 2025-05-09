ddafbfec19de63c4ea96910bb325fb855c6141cd
funchooooza-ossh <uroddaunich@gmail.com>
2025-05-10 07:32:48 +1000

    upd: another test

--- tests/unit/infrastructure/tx/test_manager.py
+++ tests/unit/infrastructure/tx/test_manager.py
@@ -20,3 +20,13 @@ async def test_transaction_manager(tx_context: SqlAlchemyTransactionContext):
     assert manager._started is False  # type: ignore
 
     assert not manager._context._session.in_transaction()  # type: ignore
+
+
+@pytest.mark.unit
+async def test_manager_double_start_raises_error(
+    tx_context: SqlAlchemyTransactionContext,
+):
+    manager = TransactionManager(context=tx_context)
+    with pytest.raises(RuntimeError, match="Transaction has already been started"):
+        await manager.start()
+        await manager.start()
